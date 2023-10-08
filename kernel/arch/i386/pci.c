afd8f2c07f9e963485503af0dcad67369a9e5157
Salmoneer <camogisan@gmail.com>
2023-10-08 11:59:57 +1100

    More PCI things (BARs might almost work)?

--- kernel/arch/i386/pci.c
+++ kernel/arch/i386/pci.c
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include <stdint.h>
+#include <stdbool.h>
 #include <kernel/io.h>
 
 uint16_t pci_config_read_word(uint8_t bus, uint8_t slot, uint8_t function, uint8_t offset) {
@@ -23,20 +24,30 @@ uint16_t pci_config_read_word(uint8_t bus, uint8_t slot, uint8_t function, uint8
 	return tmp;
 }
 
+bool pci_function_exists(uint8_t bus, uint8_t slot, uint8_t function) {
+    return !(pci_config_read_word(bus, slot, function, 0) == 0xFFFF);
+}
+
+void pci_check_function(uint8_t bus, uint8_t slot, uint8_t function) {
+	uint16_t vendor = pci_config_read_word(bus, slot, function, 0);
+	uint16_t device = pci_config_read_word(bus, slot, function, 2);
+	uint16_t class  = pci_config_read_word(bus, slot, function, 10);
+    uint16_t progif = pci_config_read_word(bus, slot, function, 8);
+	printf("%d:%d.%d:\n    [%x%x:%x%x] 0x%x.%x.%x\n", bus, slot, function, vendor >> 8, vendor, device >> 8, device, class >> 8, class, progif >> 8);
+}
+
 void pci_check_all_buses() {
 	for (uint16_t bus = 0; bus < 256; bus++) {
 		for (uint8_t slot = 0; slot < 32; slot++) {
 			uint8_t function = 0;
-
-			uint16_t vendor = pci_config_read_word(bus, slot, function, 0);
-			if (vendor == 0xFFFF) continue;
-
+            if (!pci_function_exists(bus, slot, function)) continue;
+			
 			pci_check_function(bus, slot, function);
 
 			uint16_t header = pci_config_read_word(bus, slot, function, 14);
 			if (header & 0x80) { // Multifunction, check other functions
-				for (int function = 1; function < 8; function++) {
-					if (pci_config_read_word(bus, slot, function, 0) != 0xFFFF) {
+				for (function = 1; function < 8; function++) {
+					if (pci_function_exists(bus, slot, function)) {
 						pci_check_function(bus, slot, function);
 					}
 				}
@@ -45,39 +56,26 @@ void pci_check_all_buses() {
 	}
 }
 
-void pci_check_function(uint8_t bus, uint8_t slot, uint8_t function) {
-	uint16_t vendor = pci_config_read_word(bus, slot, function, 0);
-	uint16_t device = pci_config_read_word(bus, slot, function, 2);
-	uint16_t class  = pci_config_read_word(bus, slot, function, 10);
-	printf("%d, %d, %d:\n    [%x%x:%x%x] 0x%x.%x\n", bus, slot, function, vendor >> 8, vendor, device >> 8, device, class >> 8, class);
-}
-
 struct pci_function pci_find_function(uint8_t class, uint8_t subclass) {
 	for (uint16_t bus = 0; bus < 256; bus++) {
 		for (uint8_t slot = 0; slot < 32; slot++) {
-			uint8_t function = 0;
+            uint8_t function = 0;
+            if (!pci_function_exists(bus, slot, function)) continue;
 
-			uint16_t vendor = pci_config_read_word(bus, slot, function, 0);
-			if (vendor == 0xFFFF) continue;
+            uint16_t c = pci_config_read_word(bus, slot, function, 10);
+            if (c >> 8 == class && (uint8_t)c == subclass)
+			    return (struct pci_function){.bus = bus, .slot = slot, .function = function};
 
-			uint16_t c = pci_config_read_word(bus, slot, function, 10);
+			uint16_t header = pci_config_read_word(bus, slot, 0, 14);
+            if (header & 0x80) { // Multifunction PCI device
+    		    for (function = 1; function < 8; function++) {
+			        if (!pci_function_exists(bus, slot, function)) continue;
 
-			if (c >> 8 == class && (uint8_t)c == subclass) {
-				struct pci_function out = {.bus = bus, .slot = slot, .function = function};
-				return out;
-			}
-
-			uint16_t header = pci_config_read_word(bus, slot, function, 14);
-			if (header & 0x80) { // Multifunction, check other functions
-				for (function = 1; function < 8; function++) {
-					if (pci_config_read_word(bus, slot, function, 0) != 0xFFFF) {
-						if (c >> 8 == class && (uint8_t)c == subclass) {
-							struct pci_function out = {.bus = bus, .slot = slot, .function = function};
-							return out;
-						}
-					}
-				}
-			}
+        	    	uint16_t c = pci_config_read_word(bus, slot, function, 10);
+		        	if (c >> 8 == class && (uint8_t)c == subclass)
+			            return (struct pci_function){.bus = bus, .slot = slot, .function = function};
+                }
+            }
 		}
 	}
 
@@ -86,9 +84,12 @@ struct pci_function pci_find_function(uint8_t class, uint8_t subclass) {
 }
 
 void pci_find_BARs(uint8_t bus, uint8_t slot, uint8_t function) {
-	uint32_t bars[6] = {0};
-	for (int i = 0; i < 6; i++) {
-		bars[i] = pci_config_read_word(bus, slot, function, 16 + i * 4) + (pci_config_read_word(bus, slot, function, 18 + i * 4) << 16);
-		printf("0x%x%x%x%x\n", (uint8_t)(bars[i] >> 24), (uint8_t)(bars[i] >> 16), (uint8_t)(bars[i] >> 8), (uint8_t)bars[i]);
-	}
+    uint8_t header = pci_config_read_word(bus, slot, function, 14) & 3;
+    if (header == 0) {
+	    uint32_t bars[6] = {0};
+	    for (int i = 0; i < 6; i++) {
+	    	bars[i] = pci_config_read_word(bus, slot, function, 16 + i * 4) + (pci_config_read_word(bus, slot, function, 18 + i * 4) << 16);
+	    	printf("0x%x%x%x%x\n", (uint8_t)(bars[i] >> 24), (uint8_t)(bars[i] >> 16), (uint8_t)(bars[i] >> 8), (uint8_t)bars[i]);
+	    }
+    }
 }
--- kernel/include/drivers/pci.h
+++ kernel/include/drivers/pci.h
@@ -2,6 +2,7 @@
 #define PCI_H
 
 #include <stdint.h>
+#include <stdbool.h>
 
 struct pci_function {
 	uint8_t bus;
@@ -11,9 +12,11 @@ struct pci_function {
 
 uint16_t pci_config_read_word(uint8_t bus, uint8_t slot, uint8_t function, uint8_t offset);
 
-void pci_check_all_buses(void);
+bool pci_function_exists(uint8_t bus, uint8_t slot, uint8_t function);
 
 void pci_check_function(uint8_t bus, uint8_t slot, uint8_t function);
+
+void pci_check_all_buses(void);
 
 struct pci_function pci_find_function(uint8_t class, uint8_t subclass);
 
